cc86988bd648ca1c4c8a5d8966bb82f7be5e3662
Florian Lüke <f.lueke@mesytec.com>
2023-07-07 16:47:20 +0200

    test script updates

--- extras/mcpd-cli/mpsd8_bus_capa_test.sh
+++ extras/mcpd-cli/mpsd8_bus_capa_test.sh
@@ -10,11 +10,19 @@
 
 # Env vars used by mcpd-cli
 export MCPD_ADDRESS=192.168.168.211
+export MCPD_ADDRESS=mcpd-0002
 export MCPD_ID=1
 MPSD_BUS=5
 MPSD_PULSER_ON="${MPSD_BUS} 1 2 100 on"
 MPSD_PULSER_OFF="${MPSD_BUS} 1 2 100 off"
-DAQ_DURATION=10
+# MPSD mode: for formats P and TP the MPSD can send amplitude instead of
+# position data. Note that the value is always transmitted in the 'position'
+# field of DAQ packets for tx formats P and TP.
+MPSD_MODE="0" # pos|0|amp|1
+DAQ_DURATION=5
+READOUT_ARGS=--print-event-data
+
+set -e
 
 trap "exit" INT
 
@@ -23,10 +31,21 @@ for txfmt in 1  2  4; do
     mcpd-cli scan_busses
     mcpd-cli mpsd_set_tx_format $MPSD_BUS $txfmt
     mcpd-cli set_bus_capabilities $txfmt
-    rootfile="mcpd-8_${MCPD_ADDRESS}_${MCPD_ID}-txfmt${txfmt}.root"
     mcpd-cli mpsd_set_pulser $MPSD_PULSER_ON
+
+    # Note: after the mpsd_set_mode command the mcpd-8_v2 has to send something
+    # to the mpsd for the setting to take effect. The mcpd-8_v1 does this
+    # internally.
+    mcpd-cli mpsd_set_mode $MPSD_BUS $MPSD_MODE
+
+    # Send something to the mpsd to make the setting stick when using
+    # mcpd-8_v2. In this case the gain value is set.
+    mcpd-cli mpsd_set_gain $MPSD_BUS 1 20
+
     mcpd-cli daq start
-    mcpd-cli readout --no-listfile --print-packet-summary --print-event-data --root-histo-file "$rootfile" \
+
+    rootfile="mcpd-8_${MCPD_ADDRESS}_${MCPD_ID}-txfmt${txfmt}.root"
+    mcpd-cli readout $READOUT_ARGS --no-listfile --root-histo-file "$rootfile" \
         --duration $DAQ_DURATION
     mcpd-cli daq stop
     mcpd-cli mpsd_set_pulser $MPSD_PULSER_OFF
