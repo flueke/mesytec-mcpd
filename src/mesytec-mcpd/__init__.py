a72417cd50eb908f0cbf55e6814c9cbb3b286e6d
Florian Lüke <f.lueke@mesytec.com>
2026-03-06 10:41:26 +0100

    py: clean up imports, run main() from the gui if directly called

--- src/mesytec-mcpd/__init__.py
+++ src/mesytec-mcpd/__init__.py
@@ -1,5 +1,9 @@
 # Re-export everything from the compiled extension module
 from ._mesytec_mcpd_py import *
-from . import _mesytec_mcpd_py
 
 __version__ = _mesytec_mcpd_py.__version__
+
+try:
+    from . import gui
+except ImportError:
+    pass
