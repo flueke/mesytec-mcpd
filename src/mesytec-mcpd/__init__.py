46162b90ec4f316374629187331de9c0bf9bbe37
Florian Lüke <f.lueke@mesytec.com>
2026-03-06 11:11:56 +0100

    py: call init() upon importing the mcpd lib. less burden for users

--- src/mesytec-mcpd/__init__.py
+++ src/mesytec-mcpd/__init__.py
@@ -3,6 +3,10 @@ from ._mesytec_mcpd_py import *
 
 __version__ = _mesytec_mcpd_py.__version__
 
+
+# Sets up the spdlog -> pybind11 -> logging bridge.
+_mesytec_mcpd_py.init()
+
 try:
     from . import gui
 except ImportError:
