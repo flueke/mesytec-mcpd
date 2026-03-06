7f762ef2a7226ff774d3b578f07302ae2c712952
Florian Lüke <f.lueke@mesytec.com>
2026-03-06 10:41:26 +0100

    py: clean up imports, run main() from the gui if directly called

--- src/gui/main_window.py
+++ src/gui/main_window.py
@@ -1,7 +1,10 @@
 import boost_histogram as bh
-import mesytec_mcpd as mcpd
+import mesytec_mcpd_py as mcpd
 import pyqtgraph as pg
 import numpy as np
 
 def main():
     print("Yay!")
+
+if __name__ == "__main__":
+    main()
