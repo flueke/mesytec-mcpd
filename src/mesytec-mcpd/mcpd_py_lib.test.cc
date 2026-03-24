ce18f777a77806536460c1eea9af21827fe6fb3c
Florian Lüke <florianlueke@gmx.net>
2026-03-24 23:15:18 +0100

    fix pylib tests

--- src/mesytec-mcpd/mcpd_py_lib.test.cc
+++ src/mesytec-mcpd/mcpd_py_lib.test.cc
@@ -6,6 +6,7 @@
 namespace py = pybind11;
 
 using namespace mesytec::mcpd;
+using namespace mesytec::mcpd::py_lib;
 
 // Note: always using a non-default port to _not_ receive data from MCPDs that have been left
 // running :) Real programmers (!) would abstract the socket out and run with a mock socket instead
@@ -29,7 +30,7 @@ class PythonEnvironment: public ::testing::Environment
 [[maybe_unused]] static ::testing::Environment *const python_env =
     ::testing::AddGlobalTestEnvironment(new PythonEnvironment());
 
-TEST(mcpd_py_lib, CreateDestroyBeHappy)
+TEST(mcpd_py_lib, CreateDestroy)
 {
     set_global_log_level(spdlog::level::trace);
     spdlog::set_level(spdlog::level::trace);
@@ -39,8 +40,8 @@ TEST(mcpd_py_lib, CreateDestroyBeHappy)
         Readout readout(McpdDefaultPort + 3);
 
         ASSERT_FALSE(readout.isRunning());
-        ASSERT_FALSE(readout.hasReadoutException());
-        ASSERT_TRUE(readout.getPackets().empty());
+        ASSERT_FALSE(readout.hasException());
+        ASSERT_TRUE(readout.getQueue().attr("empty")().cast<bool>());
         ASSERT_EQ(readout.getCounters().packets, 0u);
     }
 }
@@ -53,14 +54,14 @@ TEST(mcpd_py_lib, StartStopRestart)
     {
         readout.start();
         ASSERT_TRUE(readout.isRunning());
-        ASSERT_FALSE(readout.hasReadoutException());
-        ASSERT_TRUE(readout.getPackets().empty());
+        ASSERT_FALSE(readout.hasException());
+        ASSERT_TRUE(readout.getQueue().attr("empty")().cast<bool>());
         ASSERT_EQ(readout.getCounters().packets, 0u);
 
         readout.stop();
         ASSERT_FALSE(readout.isRunning());
-        ASSERT_FALSE(readout.hasReadoutException());
-        ASSERT_TRUE(readout.getPackets().empty());
+        ASSERT_FALSE(readout.hasException());
+        ASSERT_TRUE(readout.getQueue().attr("empty")().cast<bool>());
         ASSERT_EQ(readout.getCounters().packets, 0u);
     }
 }
@@ -75,8 +76,8 @@ TEST(mcpd_py_lib, StartMultiple)
     ASSERT_TRUE(readout.isRunning());
     ASSERT_FALSE(readout1.isRunning());
 
-    ASSERT_FALSE(readout.hasReadoutException());
-    ASSERT_FALSE(readout1.hasReadoutException());
+    ASSERT_FALSE(readout.hasException());
+    ASSERT_FALSE(readout1.hasException());
 
     // This will throw 'address already in use'.
     ASSERT_THROW(readout1.start(), std::system_error);
@@ -86,6 +87,6 @@ TEST(mcpd_py_lib, StartMultiple)
 
     // No readout exception should be set. It's only done if startup
     // succeeded and then during readout time an error occurs.
-    ASSERT_FALSE(readout.hasReadoutException());
-    ASSERT_FALSE(readout1.hasReadoutException());
+    ASSERT_FALSE(readout.hasException());
+    ASSERT_FALSE(readout1.hasException());
 }
